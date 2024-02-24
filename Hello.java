2e39d8f7c5e07c5208d9b1fbeb76a24c3b01a961
Ramanuj Das <ramanujds9@gmail.com>
2024-02-24 16:14:40 +0530

    updated hello.java from m1

--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
 class Hello{
     public static void main(String []args){
-        System.out.print("Hello and let's learn Git");
+        System.out.print("Hello and let's learn mastering git merge conflicts");
     }
 }
